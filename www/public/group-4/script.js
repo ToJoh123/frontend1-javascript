0c512ac7af15d576d7629a2c661f37ad9838cfd3
Claude-Marcel Loba <100008570+claudeloba@users.noreply.github.com>
2022-10-04 17:24:47 +0200

    la till sökfält

--- www/public/group-4/script.js
+++ www/public/group-4/script.js
@@ -1,37 +1,129 @@
+let fullName = document.getElementById("name");
+let discord = document.getElementById("discord");
+let github = document.getElementById("github");
+let personalityType = document.getElementById("personalityType");
+let favoritMat = document.getElementById("favoritmat");
+let birthYear = document.getElementById("birthyear");
+let zodiac = document.getElementById("zodiac");
+
 fetch('./data.json')
   .then((res) => res.json())
   .then((data) => {
+<<<<<<< HEAD
+let next = document.getElementById("next"); // created button "show next user"
+let i = 0; // We start from index 0 in data.json
+
+for (const element of data) {
+  let option = document.createElement("option");
+  let dataList = document.getElementById("userFirstName")
+  option.value = element.firstname;
+  dataList.appendChild(option);
+}  
+=======
     let next = document.getElementById("next"); // created button "show next user"
     let i = 0; // We start from index 0 in data.json
+
+    for (const element of data) {
+      let option = document.createElement("option");
+      let dataList = document.getElementById("userFirstName")
+      option.value = element.firstname;
+      dataList.appendChild(option);
+    }  
+>>>>>>> webbpage_layout
   
     next.addEventListener("click", () => { // eventlistener listening for a click on the object "next"
       i++; // increments by 1 after iterating over an index
       if (i > data.length - 1) { // if index is greater than the length of the data in data.json =>
         i = 0; // sets index to 0
       }
-          document.getElementById("name").textContent = data[i].firstname + " " + data[i].lastname; // sets fullname to the value in json and puts it on the dom using name id
-          document.getElementById("discord").textContent = data[i].discord; // using discord id to put the data from json on to the dom using .textContent
-          document.getElementById("github").href = `https://github.com/${data[i].github}`; // linked to each users github by using .href. Taking whatever each user inputs and displaying it on the dom
-          document.getElementById("github").textContent = data[i].github;
-          document.getElementById("personalityType").textContent = data[i].personalityType;
-          document.getElementById("favoritmat").textContent = data[i].mat;
-          document.getElementById("birthYear").textContent = data[i].birthYear;
-          document.getElementById("zodiac").textContent = data[i].zodiac.name + " " + data[i].zodiac.symbol;
+<<<<<<< HEAD
+fullName.textContent = data[i].firstname + " " + data[i].lastname; // sets fullname to the value in json and puts it on the dom using name id
+discord.textContent = data[i].discord; // using discord id to put the data from json on to the dom using .textContent
+github.href = `https://github.com/${data[i].github}`; // linked to each users github by using .href. Taking whatever each user inputs and displaying it on the dom
+github.textContent = data[i].github;
+personalityType.textContent = data[i].personalityType;
+favoritMat.textContent = data[i].mat;
+birthYear.textContent = data[i].birthYear;
+zodiac.textContent = data[i].zodiac.name + " " + data[i].zodiac.symbol;
+})
+
+let previous = document.getElementById("previous"); // created button "Show previous user" that lets you check prev user.
+previous.addEventListener("click", () => { // eventlistener listening to a click on previous object
+i--; // decrements, a.k.a goes backward
+if (i < 0) {
+i = data.length - 1;
+}
+fullName.textContent = data[i].firstname + " " + data[i].lastname;
+discord.textContent = data[i].discord;
+github.href = `https://github.com/${data[i].github}`;
+github.textContent = data[i].github;
+personalityType.textContent = data[i].personalityType;
+favoritMat.textContent = data[i].mat;
+birthYear.textContent = data[i].birthYear;
+zodiac.textContent = data[i].zodiac.name + " " + data[i].zodiac.symbol;
+})
+
+let search = document.getElementById("search");
+search.addEventListener("click", () => {
+let searchInput = document.getElementById("searchInput").value;
+for (const element of data) {
+if (searchInput.toLowerCase() === element.firstname.toLowerCase()) {
+fullName.textContent = element.firstname + " " + element.lastname;
+discord.textContent = element.discord;
+github.href = `https://github.com/${element.github}`;
+github.textContent = element.github;
+personalityType.textContent = element.personalityType;
+favoritMat.textContent = element.mat;
+birthYear.textContent = element.birthYear;
+zodiac.textContent = element.zodiac.name + " " + element.zodiac.symbol;
+}
+}
+})
+=======
+          fullName.textContent = data[i].firstname + " " + data[i].lastname; // sets fullname to the value in json and puts it on the dom using name id
+          discord.textContent = data[i].discord; // using discord id to put the data from json on to the dom using .textContent
+          github.href = `https://github.com/${data[i].github}`; // linked to each users github by using .href. Taking whatever each user inputs and displaying it on the dom
+          github.textContent = data[i].github;
+          personalityType.textContent = data[i].personalityType;
+          favoritMat.textContent = data[i].mat;
+          birthYear.textContent = data[i].birthYear;
+          zodiac.textContent = data[i].zodiac.name + " " + data[i].zodiac.symbol;
   })
+
   let previous = document.getElementById("previous"); // created button "Show previous user" that lets you check prev user.
   previous.addEventListener("click", () => { // eventlistener listening to a click on previous object
     i--; // decrements, a.k.a goes backward
     if (i < 0) {
       i = data.length - 1;
     }
-        document.getElementById("name").textContent = data[i].firstname + " " + data[i].lastname;
-        document.getElementById("discord").textContent = data[i].discord;
-        document.getElementById("github").href = `https://github.com/${data[i].github}`;
-        document.getElementById("github").textContent = data[i].github;
-        document.getElementById("personalityType").textContent = data[i].personalityType;
-        document.getElementById("favoritmat").textContent = data[i].mat;
-        document.getElementById("birthYear").textContent = data[i].birthYear;
-        document.getElementById("zodiac").textContent = data[i].zodiac.name + " " + data[i].zodiac.symbol;
-      })
-  
+          fullName.textContent = data[i].firstname + " " + data[i].lastname;
+          discord.textContent = data[i].discord;
+          github.href = `https://github.com/${data[i].github}`;
+          github.textContent = data[i].github;
+          personalityType.textContent = data[i].personalityType;
+          favoritMat.textContent = data[i].mat;
+          birthYear.textContent = data[i].birthYear;
+          zodiac.textContent = data[i].zodiac.name + " " + data[i].zodiac.symbol;
+        })
+
+  let search = document.getElementById("search");
+  search.addEventListener("click", () => {
+    let searchInput = document.getElementById("searchInput").value;
+    for (const element of data) {
+      if (searchInput.toLowerCase() === element.firstname.toLowerCase()) {
+        fullName.textContent = element.firstname + " " + element.lastname;
+        discord.textContent = element.discord;
+        github.href = `https://github.com/${element.github}`;
+        github.textContent = element.github;
+        personalityType.textContent = element.personalityType;
+        favoritMat.textContent = element.mat;
+        birthYear.textContent = element.birthYear;
+        zodiac.textContent = element.zodiac.name + " " + element.zodiac.symbol;
+      }
+    }
+  })
+>>>>>>> webbpage_layout
 }).catch((err) => console.log(err));
+
+
+
